e5f43dbc6702206db763b4dbe0a38feac80fed02
Alexander Paz <alexjpaz@gmail.com>
2017-06-10 18:41:48 -0400

    fixup! Level 2

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/Miserlou/dnd-tldr
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Character = require('./lib/character');
+
 const fs = require('fs');
 const dust = require('dustjs-linkedin');
 
@@ -7,60 +9,10 @@ dust.loadSource(compiled);
 
 const yaml = require('js-yaml');
 
-const character= yaml.safeLoad(fs.readFileSync('./characters/kriv/basic.yml', 'utf8'));
+const data = yaml.safeLoad(fs.readFileSync('./characters/kriv/basic.yml', 'utf8'));
 
-character.abilities_list = Object.keys(character.abilities).map(k => {
-    return Object.assign({ 
-        ability: k,
-        proficient: character.profenciences.includes(k),
-    }, character.abilities[k]);
-});
+var character = new Character(data);
 
-const abilities = [
-    "Strength",
-    "Dexterity",
-    "Constitution",
-    "Intelligence",
-    "Wisdom",
-    "Charisma",
-];
-
-character.saving_throws = abilities.map((ability) => {
-    return {
-        ability: ability,
-        mod: character.abilities[ability].mod,
-        proficient: character.profenciences.includes(ability)
-    }
-});
-
-const skills = {
-    Acrobatics: "Dexterity",
-    AnimalHandling: "Wisdom",
-    Arcana: "Intelligence",
-    Athletics: "Strength",
-    Deception: "Charisma",
-    History: "Intelligence",
-    Insight: "Wisdom",
-    Intimidation: "Charisma",
-    Investigation: "Intelligence",
-    Medicine: "Wisdom",
-    Nature: "Intelligence",
-    Perception: "Wisdom",
-    Performance: "Charisma",
-    Persuasion: "Charisma",
-    Religion: "Intelligence",
-    SleightOfHand: "Dexterity",
-    Stealth: "Dexterity",
-    Survival: "Wisdom",
-};
-
-character.skills = Object.keys(skills).map((key) => {
-    return {
-        skill: key, 
-        mod: character.abilities[skills[key]].mod + character.profeciency_bonus, 
-        proficient: character.profenciences.includes(key)
-    };
-});
 
 dust.render('character_sheet', character, function(err, out) {
     fs.writeFileSync('./out.html', out);
